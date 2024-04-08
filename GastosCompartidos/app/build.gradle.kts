4fa430b347bcd9357519056d9f17d8aa64b94dd8
Jairo Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2024-04-08 16:25:17 -0600

    agregamos libreria y creamos estilo

--- GastosCompartidos/app/build.gradle.kts
+++ GastosCompartidos/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -36,6 +37,12 @@ android {
 }
 
 dependencies {
+
+    //librerias de firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation ("com.google.firebase:firebase-auth-ktx:22.3.1")
+    implementation ("com.google.firebase:firebase-core:21.1.1")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
