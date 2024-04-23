2e2567fb9261a4cd5d158e23e6273c1675596ebb
Jairo Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2024-04-23 13:53:34 -0600

    crear nuevo evento

--- GastosCompartidos/app/src/main/java/com/strainteam/gastoscompartidos/view/home/fragments/HomeFragment.kt
+++ GastosCompartidos/app/src/main/java/com/strainteam/gastoscompartidos/view/home/fragments/HomeFragment.kt
@@ -5,12 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.strainteam.gastoscompartidos.R
+import com.strainteam.gastoscompartidos.databinding.DialogNewEventsBinding
 import com.strainteam.gastoscompartidos.databinding.FragmentHomeBinding
+import com.strainteam.gastoscompartidos.viewmodel.home.fragments.HomeFragViewModel
 
 
 class HomeFragment : Fragment() {
     private lateinit var binding : FragmentHomeBinding
+    private val viewModel: HomeFragViewModel by viewModels()
+    private var motivoList = ArrayList<String>()
+    private var cuotaList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +34,38 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //observadores
+        viewModel.motivoList.observe(viewLifecycleOwner, Observer {
+            motivoList = it
+        })
+
+        viewModel.cuotaList.observe(viewLifecycleOwner, Observer {
+            cuotaList = it
+        })
+
+        viewModel.messageToast.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(requireContext(),it,Toast.LENGTH_LONG).show()
+        })
+
+        viewModel.showDialogEvent.observe(viewLifecycleOwner, Observer {
+            val dialog = MaterialAlertDialogBuilder(requireContext())
+            val bindingDialog = DialogNewEventsBinding.inflate(layoutInflater)
+            dialog.setView(bindingDialog.root)
+            dialog.setCancelable(false)
+            bindingDialog.spTipoEvento.adapter = ArrayAdapter(requireContext(), R.layout.spinner_list,R.id.tvTextSpinner, motivoList)
+            bindingDialog.spTipoCuota.adapter = ArrayAdapter(requireContext(), R.layout.spinner_list,R.id.tvTextSpinner, cuotaList)
+            val builder = dialog.create()
+            builder.show()
+            bindingDialog.btnCancel.setOnClickListener {
+                builder.dismiss()
+            }
+        })
+
+        //evento a click
+        binding.tvNuevoEvento.setOnClickListener {
+            viewModel.getTipoCuota()
+        }
     }
 
 }
--- GastosCompartidos/app/src/main/java/com/strainteam/gastoscompartidos/viewmodel/home/fragments/HomeFragViewModel.kt
+++ GastosCompartidos/app/src/main/java/com/strainteam/gastoscompartidos/viewmodel/home/fragments/HomeFragViewModel.kt
@@ -2,7 +2,57 @@ package com.strainteam.gastoscompartidos.viewmodel.home.fragments
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class HomeFragViewModel(application: Application): AndroidViewModel(application) {
     private val context = getApplication<Application>().applicationContext
+    private lateinit var auth: FirebaseAuth
+    private lateinit var dbMotivoRef: DatabaseReference
+    private lateinit var dbCuotaRef: DatabaseReference
+    private lateinit var database: FirebaseDatabase
+    val motivoList = MutableLiveData<ArrayList<String>>()
+    val cuotaList = MutableLiveData<ArrayList<String>>()
+    val messageToast = MutableLiveData<String>()
+    val showDialogEvent = MutableLiveData<Boolean>()
+
+    init {
+        database = FirebaseDatabase.getInstance()
+        auth = FirebaseAuth.getInstance()
+        dbMotivoRef = database.reference.child("MotivosEventos")
+        dbCuotaRef = database.reference.child("TipoCuota")
+    }
+
+    fun getMotivosEventos(){
+        dbMotivoRef.get().addOnCompleteListener {
+            if(it.isSuccessful){
+                val motivosList = ArrayList<String>()
+                for (motivo in it.result!!.children){
+                    motivosList.add("${motivo.key} - ${motivo.value}")
+                }
+                motivoList.value = motivosList
+                showDialogEvent.value = true
+            }else{
+                messageToast.value = "Error motivos de eventos: ${it.exception?.message}"
+            }
+        }
+    }
+
+    fun getTipoCuota(){
+        dbCuotaRef.get().addOnCompleteListener {
+            if(it.isSuccessful){
+                val cuotasList = ArrayList<String>()
+                for (cuota in it.result!!.children){
+                    cuotasList.add("${cuota.key} - ${cuota.value}")
+                }
+                cuotaList.value = cuotasList
+                getMotivosEventos()
+            }else{
+                messageToast.value = "Error tipo de cuota: ${it.exception?.message}"
+            }
+        }
+    }
+
 }
